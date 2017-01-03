cccd93767589e593fb3e7e840777ba35a9ee438b
侯进10105761 <hou.jin@zte.com.cn>
2017-01-03 15:06:50 +0800

    迭代

--- 1-12.scm
+++ 1-12.scm
@@ -4,3 +4,17 @@
                     ((or (= p 1) (= p n)) 1)                   
                     (else (+ (pascal (- p 1) (- n 1)) (pascal p (- n 1))))))
 
+;f(p,n)=n!/p!(n-p)!
+(define (factorial-i a b count)
+            (if (> b count) 
+                 a
+                 (factorial-i (* a b) (+ b 1) count)))
+
+(define (factorial n)
+            (factorial-i 1 1 n))
+
+(define (pascal2 p n)
+            (/ (factorial n)
+                  (* (factorial p) (factorial (- n p)))))
+
+      
