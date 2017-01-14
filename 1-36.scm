7715eb9eae5e0f823c012242f06f52ec004fa9f4
侯进10105761 <hou.jin@zte.com.cn>
2017-01-14 12:09:53 +0800

    1-36

--- 1-36.scm
+++ 1-36.scm
@@ -1,14 +1,16 @@
 (define (check-close next guest)
         (< (abs (- next guest)) 0.001))
 
-(define (fixed-point f first-guest)
+(define (fixed-point f first-guest)  
         (define (try-it f guest)
+                (display guest)
+                (newline)
                 (let ((next (f guest)))
                      (if (check-close next guest)
                          guest
                          (try-it f next))))
        (try-it f first-guest))
 
+(fixed-point (lambda (x) (/ (log 1000) (log x))) (exact->inexact 2))
 
-(fixed-point (lambda (x) (+ 1 (/ 1 x))) (exact->inexact 1))
-                
+(fixed-point (lambda (x) ((lambda (a b) (/ (+ a b) 2)) x (/ (log 1000) (log x)))) (exact->inexact 2))
