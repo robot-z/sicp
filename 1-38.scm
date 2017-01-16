a9cdd5645d715dba3f2acd8e8eada82cf5314008
侯进10105761 <hou.jin@zte.com.cn>
2017-01-16 14:16:34 +0800

    1-38

--- 1-38.scm
+++ 1-38.scm
@@ -2,4 +2,8 @@
         (let ((rem (remainder i 3)))
              (if (or (= rem 1) (= rem 0))
                  1
-                 (* (+ (/ i 3) 1) 2))))
+                 (* (/ (+ i 1) 3) 2))))
+
+(+ (cont-frac (lambda (x) 1.0)
+           d
+           100) 2)
